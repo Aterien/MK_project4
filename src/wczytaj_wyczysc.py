5b1b6f07d3562e75106ef2ed8777d8b96a6069de
Zofia Rewak <zofiarewak@device-4.home>
2026-01-15 00:08:38 +0100

    Zaaktualizowałam pliki do wczytywania i analizy

--- src/wczytaj_wyczysc.py
+++ src/wczytaj_wyczysc.py
@@ -3,21 +3,7 @@ import requests
 import zipfile
 import io
 
-gios_archive_url = "https://powietrze.gios.gov.pl/pjp/archives/downloadFile/"
-gios_url_ids = {
-    2015: 'XXX',  # podmień na właściwe ID
-    2018: 'YYY',
-    2021: 'ZZZ',
-    2024: '582'
-}
-gios_pm25_file = {
-    2015: '2015_PM2.5_1g.xlsx',
-    2018: '2018_PM2.5_1g.xlsx',
-    2021: '2021_PM2.5_1g.xlsx',
-    2024: '2024_PM25_1g.xlsx'
-}
-
-def download_gios_archive(year, gios_id, filename):
+def download_gios_archive(gios_archive_url, gios_id, filename):
     response = requests.get(f"{gios_archive_url}{gios_id}")
     response.raise_for_status()
     with zipfile.ZipFile(io.BytesIO(response.content)) as z:
@@ -25,14 +11,45 @@ def download_gios_archive(year, gios_id, filename):
             df = pd.read_excel(f, header=None)
     return df
 
-def ujednolic_dane(tabela):
+def download_metadata(gios_archive_url,metadata_url_id):
+    # Pobranie metadanych
+    url = f"{gios_archive_url}{metadata_url_id}"
+    response = requests.get(url)
+    response.raise_for_status()  # jeśli błąd HTTP, zatrzymaj
+
+    try:
+        df = pd.read_excel(io.BytesIO(response.content),engine='openpyxl')
+        return df
+    except Exception as e:
+        print(f"Błąd przy wczytywaniu metadanych: {e}")
+        return None
+
+def zaktualizuj_nazwy_stacji(df,metadane):
+    # Teraz chcę zmienić kody stacji, tak aby były aktualne na podstawie pliku metadane:
+    # Tworzę słownik kodów- będzie działał na zasadzie stary kod: nowy kod.
+    # W ten sposób mogę potem łatwo aktualizować kody stacji.
+    slownik_kodow = {}
+    for _, row in metadane.iterrows():
+        stary_kod = row['Stary Kod stacji \n(o ile inny od aktualnego)']
+        nowy_kod = row["Kod stacji"]
+        if pd.notna(stary_kod):
+            # Starych kodów może być też kilka:
+            stary_kod = stary_kod.split(",")
+            for s in stary_kod:
+                slownik_kodow[s] = nowy_kod
+
+    df.rename(columns=slownik_kodow, inplace=True)
+    return df
+
+def ujednolic_dane(tabela,metadane):
     tabela = tabela.copy()
     tabela = tabela[~tabela.iloc[:,0].isin(['Wskaźnik','Czas uśredniania','Jednostka', 'Kod stanowiska', 'Nr'])]
     tabela.columns = tabela.iloc[0]
     tabela = tabela.drop(tabela.index[0]).reset_index(drop=True)
     tabela = tabela.rename(columns={"Kod stacji": "Data poboru danych"})
     tabela = tabela.set_index('Data poboru danych')
-    return tabela
+    tabela = zaktualizuj_nazwy_stacji(tabela,metadane)
+    return tabela 
 
 def wspolne_stacje(df_list):
     wsp = df_list[0].columns
@@ -48,6 +65,7 @@ def multiindex_funkcja(df, metadane, wsp_stacje):
 
 def przesun_date(df):
     df = df.copy()
+    df.index = pd.to_datetime(df.index, errors="coerce",format="%Y-%m-%d %H:%M:%S.%f")
     polnoc = df.index.hour == 0
     nowy_indeks = [t - pd.Timedelta(seconds=1) if h else t for t, h in zip(df.index, polnoc)]
     df.index = pd.DatetimeIndex(nowy_indeks)
